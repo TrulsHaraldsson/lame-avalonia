f9da9b4abf9d68caed509b13595698c9465622c5
Truls Haraldsson <truls.haraldsson@gmail.com>
2025-09-14 19:50:27 +0200

    LoginViewModel added

---
+++ Lame/Lame.Test/ViewModels/TestLoginViewModel.cs
@@ -0,0 +1,93 @@
+﻿using Lame.Services;
+using Lame.ViewModels;
+using Moq;
+
+namespace Lame.Test.ViewModels;
+
+public class TestLoginViewModel
+{
+    [TestCase("", "", false)]
+    [TestCase("truls", "", false)]
+    [TestCase("", "password", false)]
+    [TestCase("test", "password", true)]
+    public void Login_Is_Only_Available_When_UserName_And_Password_Are_Set(string username, string password,
+        bool expected)
+    {
+        // Arrange
+        var moq = new Mock<IAuthService>();
+        var vm = new LoginViewModel(moq.Object);
+
+        // Act
+        vm.Username = username;
+        vm.Password = password;
+        
+        // Assert
+        Assert.That(vm.LoginIsAvailable, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Pressing_Login_Calls_AuthService()
+    {
+        // Arrange
+        var moq = new Mock<IAuthService>();
+        var vm = new LoginViewModel(moq.Object);
+        
+        // Act
+        vm.LoginCommand.Execute().Subscribe();
+        
+        // Assert
+        moq.Verify(m => m.TryLogin(It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+    }
+    
+    [Test]
+    public void Login_Command_Calls_TryLogin_With_Correct_Parameters()
+    {
+        // Arrange
+        var moq = new Mock<IAuthService>();
+        var vm = new LoginViewModel(moq.Object);
+        
+        // Act
+        vm.Username = "truls";
+        vm.Password = "password";
+        vm.LoginCommand.Execute().Subscribe();
+        
+        // Assert
+        moq.Verify(m => m.TryLogin("truls", "password"), Times.Once);
+    }
+    
+    [Test]
+    public void Login_Command_Is_Enabled_When_Username_And_Password_Are_Set()
+    {
+        // Arrange
+        var moq = new Mock<IAuthService>();
+        var vm = new LoginViewModel(moq.Object);
+        bool captured = false;
+        vm.LoginCommand.CanExecute.Subscribe(v =>
+        {
+            captured = v;
+        });
+        
+        // Act
+        vm.Username = "truls";
+        vm.Password = "password";
+        
+        // Assert
+        Assert.That(captured, Is.True);
+    }
+
+    [Test]
+    public void Register_Command_Calls_Register_With_Correct_Parameters()
+    {
+        // Arrange
+        var moq = new Mock<IAuthService>();
+        var vm = new LoginViewModel(moq.Object);
+        vm.Username = "truls";
+        vm.Password = "password";
+        
+        // Act
+        vm.RegisterCommand.Execute().Subscribe();
+        
+        // Assert
+        moq.Verify(m => m.RegisterAsync("truls", "password"), Times.Once);
+    }
+}
---
+++ Lame/Lame/ViewModels/LoginViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+using Lame.Services;
+using ReactiveUI;
+
+namespace Lame.ViewModels;
+
+public class LoginViewModel : ViewModelBase, IActivatableViewModel
+{
+    private readonly IAuthService _authService;
+    
+    private string _username = string.Empty;
+    public string Username
+    {
+        get => _username; 
+        set => this.RaiseAndSetIfChanged(ref _username, value);
+    }
+    
+    private string _password = string.Empty;
+    public string Password
+    {
+        get => _password; 
+        set => this.RaiseAndSetIfChanged(ref _password, value);
+    }
+
+    private readonly ObservableAsPropertyHelper<bool> _loginIsAvailable;
+    public bool LoginIsAvailable => _loginIsAvailable.Value;
+    
+    public ReactiveCommand<Unit, bool> LoginCommand { get; }
+    public ReactiveCommand<Unit, bool> RegisterCommand { get; }
+
+    public LoginViewModel(IAuthService authService)
+    {
+        Activator = new ViewModelActivator();
+        _authService = authService;
+        _loginIsAvailable = this.WhenAnyValue(vm => vm.Username, vm => vm.Password)
+            .Select(s => !string.IsNullOrEmpty(s.Item1) && !string.IsNullOrEmpty(s.Item2))
+            .ToProperty(this, vm => vm.LoginIsAvailable);
+        
+        LoginCommand = ReactiveCommand.Create(
+            () => _authService.TryLogin(Username, Password),
+            this.WhenAnyValue(x => x.LoginIsAvailable)
+        );
+
+        RegisterCommand = ReactiveCommand.CreateFromTask(
+            () => _authService.RegisterAsync(Username, Password),
+            this.WhenAnyValue(x => x.LoginIsAvailable));
+    }
+
+    public ViewModelActivator Activator { get; }
+}
