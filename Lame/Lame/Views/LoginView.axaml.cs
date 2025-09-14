9341de48c8201de7a4a4ac9efad76561f1ce373e
Truls Haraldsson <truls.haraldsson@gmail.com>
2025-09-14 20:05:50 +0200

    LoginView added

---
+++ Lame/Lame/Views/LoginView.axaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Reactive.Disposables;
+using Avalonia.ReactiveUI;
+using Lame.ViewModels;
+using ReactiveUI;
+
+namespace Lame.Views;
+
+public partial class LoginView : ReactiveUserControl<LoginViewModel>
+{
+    public LoginView()
+    {
+        InitializeComponent();
+        
+        this.WhenActivated(disposables =>
+        {
+            this.BindCommand(ViewModel, vm => vm.LoginCommand, v => v.LoginButton)
+                .DisposeWith(disposables);
+            
+            this.Bind(ViewModel, vm => vm.Username, v => v.UserNameTextBox.Text)
+                .DisposeWith(disposables);
+            
+            this.Bind(ViewModel, vm => vm.Password, v => v.PasswordTextBox.Text)
+                .DisposeWith(disposables);
+        });
+    }
+}
